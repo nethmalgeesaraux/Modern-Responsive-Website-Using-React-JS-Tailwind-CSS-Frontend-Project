365935fa1e25acca919ef7d4bbd56c2f31a4f4b0
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-05 06:08:54 +0530

    Components Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { Hero } from "./components/Hero";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero/>
     </div>
   );
 }
