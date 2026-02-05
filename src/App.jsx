97e65b51b10bc3251178f9f8cd9ce9c7e20a4077
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-05 05:31:45 +0530

    Components Navber

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+
 
 function App() {
+  const [theme, setTheme] = React.useState("light");
+
   return (
-    <div>
-      
+    <div className="dark:bg-black relative">
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
 
-export const Navbar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+const Navbar = ({ theme }) => {
+    const logoSrc = theme === "dark" ? assets.logo_dark : assets.logo;
+
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    return (
+        <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
+            <img
+                src={logoSrc}
+                alt="Logo"
+                className="w-32 sm:w-40"
+            />
+
+            <div className={`text-gray-700 dark:text-white sm:text-sm 
+                     ${!isMenuOpen ? `max-sm:w-0 overflow-hidden` : `max-sm:w-60 max-sm:pl-10`} max-sm:fixed top-0 bottom-0 right-0 
+                      max-sm:min-h-screen max-sm:h-full max-sm:flex-col 
+                      max-sm:bg-primary max-sm:text-white max-sm:pt-20 
+                      flex sm:items-center gap-5 transition-all`}>
+
+                <img
+                    src={assets.close_icon}
+                    alt=""
+                    className="w-5 absolute right-4 top-4 sm:hidden" onClick={() => setIsMenuOpen(false)}
+                />
+
+                <a onClick={() => setIsMenuOpen(false)} href="#" className="sm:hover:border-b">Home</a>
+                <a onClick={() => setIsMenuOpen(false)} href="#services" className="sm:hover:border-b">Services</a>
+                <a onClick={() => setIsMenuOpen(false)} href="#our-work" className="sm:hover:border-b">Our Work</a>
+                <a onClick={() => setIsMenuOpen(false)} href="#contact-us" className="sm:hover:border-b">Contact Us</a>
+            </div>
+
+            <div className="flex items-center gap-2 sm:gap-4">
+
+                <img
+                    src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon}
+                    alt=""
+                    onClick={() => setIsMenuOpen(true)}
+                    className="w-8 sm:hidden"
+                />
+
+
+                <a href="#contact-us"
+                    className="text-sm max-sm:hidden flex items-center gap-2 
+                            bg-primary text-white px-6 py-2 rounded-full 
+                            cursor-pointer hover:scale-103 transition-all"
+                >
+                    Connect
+                    <img src={assets.arrow_icon} width={14} alt="" />
+                </a>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Navbar;
