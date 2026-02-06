8c0220bf5e84f93273136b45cc4fb4a25b443d60
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-06 17:13:24 +0530

    update all

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { TrustedBy } from "./components/TrustedBy";
@@ -7,24 +7,54 @@ import { OurWork } from "./components/OurWork";
 import { Teams } from "./components/Teams";
 import { ContactUs } from "./components/ContactUs";
 import { Footer } from "./components/Footer";
-// import { Toaster } from "react-hot-toast";
-
 
 function App() {
-  const [theme, setTheme] = React.useState("light");
+  const [theme, setTheme] = useState("light");
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setPos({ x: e.clientX, y: e.clientY });
+  };
 
   return (
-    <div className="dark:bg-black relative">
-      {/* <Toaster/> */}
+    <div
+      className="dark:bg-black relative min-h-screen"
+      onMouseMove={handleMouseMove}
+    >
+      {/* Overlay layer for mouse effect */}
+      <div className="pointer-events-none fixed inset-0 z-50">
+        {/* Dot */}
+        <div
+          className="absolute w-4 h-4 bg-blue-500 rounded-full"
+          style={{
+            left: pos.x - 8,
+            top: pos.y - 8,
+            transition: "transform 0.05s linear",
+          }}
+        ></div>
+
+        {/* Ring */}
+        <div
+          className="absolute w-16 h-16 border border-gray-400 rounded-full"
+          style={{
+            left: pos.x - 32,
+            top: pos.y - 32,
+            transition: "transform 0.1s linear",
+          }}
+        ></div>
+
+      </div>
+
+      {/* Main sections */}
       <Navbar theme={theme} setTheme={setTheme} />
-      <Hero/>
-      <TrustedBy/>
-      <Services/>
-      <OurWork/>
-      <Teams/>
-      <ContactUs/>
-      <br/>
-      <Footer theme={theme}/>
+      <Hero />
+      <TrustedBy />
+      <Services />
+      <OurWork />
+      <Teams />
+      <ContactUs />
+      <br />
+      <Footer theme={theme} />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import assets from '../assets/assets'
+import "./Hero.css"
 
 export const Hero = () => {
     return (
